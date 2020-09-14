624b82c2c89d995592205c026ad65023659825a2
Shawn Sullivan <sullivansm3@gmail.com>
2020-09-14 10:59:13 -0600

    Render data from the global app data context

--- src/AppStateContext.tsx
+++ src/AppStateContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react"
+import React, { createContext, useReducer, useContext } from "react"
 
 interface Task { 
     id: string 
@@ -47,5 +47,9 @@ export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
             {children}
         </AppStateContext.Provider> 
     )
+}
+
+export const useAppState = () => { 
+    return useContext(AppStateContext)
 }
     

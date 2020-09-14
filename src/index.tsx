11c37cfa8b4319ea6217732fc486825ce072cd62
Shawn Sullivan <sullivansm3@gmail.com>
2020-09-14 10:49:38 -0600

    Create global state. Wrap app in context provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AppStateProvider } from "./AppStateContext"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AppStateProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AppStateProvider>,
   document.getElementById('root')
 );

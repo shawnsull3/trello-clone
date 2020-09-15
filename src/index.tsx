26a16c0381d0232a4968bb03b36156b9ee92c98a
Shawn Sullivan <sullivansm3@gmail.com>
2020-09-15 09:13:16 -0600

    Add reaect-DnD to application

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { DndProvider } from "react-dnd"
+import { HTML5Backend as Backend} from 'react-dnd-html5-backend'
 import { AppStateProvider } from "./AppStateContext"
 
 ReactDOM.render(
-  <AppStateProvider>
-    <React.StrictMode>
+  <DndProvider backend={Backend}>
+    <AppStateProvider>
       <App />
-    </React.StrictMode>
-  </AppStateProvider>,
+    </AppStateProvider>
+  </DndProvider>,
   document.getElementById('root')
 );
